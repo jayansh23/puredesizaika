c3a057131b03a24cd269bf213b751ad3931bb2f4
unknown <pooja21rak@gmail.com>
2023-09-10 11:00:00 -0400

    removed extra lines from index.js

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ import "slick-carousel/slick/slick-theme.css";
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
